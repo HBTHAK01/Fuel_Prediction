35e5fa7faf20887788fc9d7733c001ae1bc1be74
ChaseHenderson-git <79380509+ChaseHenderson-git@users.noreply.github.com>
2022-02-13 22:43:06 -0600

    Commit revert

--- fuel_app/views.py
+++ fuel_app/views.py
@@ -8,13 +8,3 @@ views = Blueprint("views", __name__)
 @views.route('/')
 def index():
 	return render_template("index.html")
-
-#Create Account Page Route
-@views.route('/create')
-def create():
-	return render_template("accountCreate.html")
-
-#Profile Managment Page Route
-@views.route('/profile')
-def profile():
-	return render_template("profile.html")
