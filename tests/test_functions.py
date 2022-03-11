2a782bc49b81056fef65861925ac35bf57937949
jcrz28 <cruzjohnleonard@gmail.com>
2022-03-11 11:07:55 -0600

    FPA-19 # comment Sign Up Unit Test

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,5 +1,14 @@
 # Functions to test all the functions of the application
 
+# Testing createAccount function
+def test_createAccount(client):
+    response = client.post("/createAccount", data ={
+        "username_Create": "testUser",
+        "password_Create": "abcABC123",
+        "password_Confirm": "abcABC123",
+    })
+    assert response.status_code == 200
+
 # Testing forgotPassword function
 def test_forgotPassword(client):
     response = client.post("/forgotPassword", data ={
