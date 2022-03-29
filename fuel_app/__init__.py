333db2ae55163226c4a0806adad89c790eb21e1c
Het Thakkar <77647228+HBTHAK01@users.noreply.github.com>
2022-03-29 16:14:21 -0500

    Adjustment of menu items and setting up Email server

--- fuel_app/__init__.py
+++ fuel_app/__init__.py
@@ -2,9 +2,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_mail import Mail, Message
 
 # Create db object
 db = SQLAlchemy()
+mail = Mail()
 
 # Using pymysql connector to use for SQLALCHEMY
 conn = 'mysql+pymysql://root:****@localhost/Fuel_Database'
@@ -22,12 +24,25 @@ def create_app():
 
     # Importing models
     from .models import Usercredentials, Clientinformation, Fuelquote
+
     # Connecting SQLALCHEMY to pymysql
     app.config['SQLALCHEMY_DATABASE_URI'] = conn
 
     # Binding db to app
     db.init_app(app)
     
+    # Configure server parameters
+    app.config['MAIL_SERVER']='smtp.gmail.com'
+    app.config['MAIL_PORT'] = 465
+    app.config['MAIL_USERNAME'] = 'fuelapp03@gmail.com'
+    app.config['MAIL_PASSWORD'] = 'jhivvglwujhkldeb'
+    app.config['MAIL_USE_TLS'] = False
+    app.config['MAIL_USE_SSL'] = True
+
+    # Create an instance of the Mail class
+    mail.init_app(app)
+
+
     login_manager = LoginManager()
 
     # Default url
