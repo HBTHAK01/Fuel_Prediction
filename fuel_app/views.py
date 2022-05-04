b207a8ece37d7705c11b61bc6181068328f2707a
Het Thakkar <77647228+HBTHAK01@users.noreply.github.com>
2022-05-04 09:39:12 -0500

    FuelQuote and FuelHistory Work

--- fuel_app/views.py
+++ fuel_app/views.py
@@ -1,5 +1,7 @@
 # import statements
 from re import A
+
+from pytest import Session
 from flask import Blueprint, message_flashed, render_template, request, flash, redirect, url_for
 from flask_login import login_user, login_required, logout_user, current_user
 from flask_mail import Mail, Message
@@ -7,11 +9,60 @@ from .models import Usercredentials, Clientinformation, Fuelquote
 from . import db
 from . import mail
 
+
 # Hash passwords
 from werkzeug.security import generate_password_hash, check_password_hash
 
+
+
+
+
+
 # Defining blueprint named "views"
 views = Blueprint("views", __name__)
+
+
+
+
+
+#####################################
+class pricing_module:
+    def __init__(self, gallons_requested, isTexas, hasHistory, current_price = 1.50, company_profit = 0.1):
+        self.gallons_requested = float(gallons_requested)
+        self.current_price = current_price
+        self.isTexas = isTexas
+        self.hasHistory = hasHistory
+        self.company_profit = company_profit
+
+    def get_quote(self):
+        if self.isTexas:
+            self.location_factor = 0.02
+        else:
+            self.location_factor = 0.04
+
+        if self.hasHistory:
+            self.rate_history_factor = 0.01
+        else:
+            self.rate_history_factor = 0.00
+
+        gallons_requested_factor = 0.02
+        if self.gallons_requested < 1000:
+            gallons_requested_factor = 0.03
+        
+        margin = self.current_price * (self.location_factor - self.rate_history_factor + gallons_requested_factor + self.company_profit)
+        # print(margin)
+        suggested_price = self.current_price + margin
+        total_amt_due = self.gallons_requested * suggested_price
+
+        return (suggested_price, total_amt_due)
+##################################################
+
+
+
+
+
+
+
 
 # Home Page Route
 @views.route('/', methods = ['GET','POST'])
@@ -99,7 +150,7 @@ def profile():
 		state = request.form.get ("select-state")
 		zipcode = request.form.get ("zipcode_Profile")
 
-		new_profile = Clientinformation(name = name, email = email, address1 = address1, address2 = address2, city = city, state = state, zipcode = zipcode)
+		new_profile = Clientinformation(userid = current_user.username, name = name, email = email, address1 = address1, address2 = address2, city = city, state = state, zipcode = zipcode)
 		db.session.add(new_profile)
 		db.session.commit()
 
@@ -117,21 +168,75 @@ def profile():
 @login_required
 def fuelQuote():
 	if request.method == "POST":
-		gallons = request.form.get ("gallons_requested")
-		deliverydate = request.form.get ("delivery_date")
+		if request.form["submit_button"] == "getquote":
+			# print(1)
+			gallons = request.form.get ("gallons_requested")
+			deliverydate = request.form.get ("delivery_date")
 
-		new_quote = Fuelquote(gallons = gallons, deliverydate = deliverydate)
-		db.session.add(new_quote)
-		db.session.commit()
-		flash ("Entry added successfully", category = "success")
 
-	return render_template("fuelQuote.html", account = current_user)
+
+			
+
+
+
+			user_find = Clientinformation.query.filter_by (userid = current_user.username).first()
+
+			if user_find.address2:
+				user_address = user_find.address1 + ", " + user_find.address2 + ", " + user_find.city + ", " + user_find.state + ", " + user_find.zipcode	
+			else:
+				user_address = user_find.address1 + ", " + user_find.city + ", " + user_find.state + ", " + user_find.zipcode
+
+			
+			if user_find.state == "TX":
+				isTexas = True
+			else:
+				isTexas = False
+
+			if Fuelquote.query.filter_by (userid = current_user.username).first():
+				hasHistory = True
+			else:
+				hasHistory = False
+
+			print(isTexas, hasHistory)
+			price_module = pricing_module(gallons, isTexas, hasHistory, current_price = 1.50, company_profit = 0.1)
+			pergallon,total = price_module.get_quote()
+
+			return render_template("fuelQuote.html", gallons = gallons, user_address = user_address, deliverydate = deliverydate, account = current_user, pergallon = pergallon, total = total)
+
+
+
+
+		elif request.form["submit_button"] == "submitquote":
+			
+			gallons = request.form.get ("gallons_requested")
+			deliveryaddress = request.form.get ("delivery_address")
+			deliverydate = request.form.get ("delivery_date")
+			pergallon = request.form.get ("price_rate")
+			total = request.form.get ("total_price")
+
+			new_quote = Fuelquote(userid = current_user.username, gallons = gallons, deliveryaddress = deliveryaddress, deliverydate = deliverydate, pergallon = pergallon, total = total)
+			db.session.add(new_quote)
+			db.session.commit()
+			flash ("Entry added successfully", category = "success")
+
+	user_find = Clientinformation.query.filter_by (userid = current_user.username).first()
+
+	if user_find.address2:
+		user_address = user_find.address1 + ", " + user_find.address2 + ", " + user_find.city + ", " + user_find.state + ", " + user_find.zipcode	
+	else:
+		user_address = user_find.address1 + ", " + user_find.city + ", " + user_find.state + ", " + user_find.zipcode
+	
+	return render_template("fuelQuote.html", account = current_user, user_address = user_address)
 
 # Fuel Quote History Page Route
 @views.route('/quoteHistory')
 @login_required
 def quoteHistory():
-	return render_template("quoteHistory.html", account = current_user)
+
+	fuel_history = Fuelquote.query.filter_by (userid = current_user.username).all()
+	
+
+	return render_template("quoteHistory.html", account = current_user, fuel_history = fuel_history)
 
 @views.route('/contact',  methods = ['GET', 'POST'])
 @login_required
